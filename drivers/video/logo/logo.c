b50a19ed8190f08183cb4498564755b2561c388c
fefifofum <fefifofum@email.com>
2014-01-13 00:59:51 +0100

    Add bq Edison 2 QC splash screen

--- drivers/video/logo/logo.c
+++ drivers/video/logo/logo.c
@@ -84,6 +84,10 @@ const struct linux_logo * __init_refok fb_find_logo(int depth)
 		/* Generic Linux logo */
 		logo = &logo_linux_clut224;
 #endif
+#ifdef CONFIG_LOGO_EDISON2QC_CLUT224
+		/* Edison 2 Quad Core logo */
+		logo = &logo_edison2qc_clut224;
+#endif
 #ifdef CONFIG_LOGO_PIPO_CLUT224
 		/* Generic Linux logo */
 		logo = &logo_pipo_clut224;
